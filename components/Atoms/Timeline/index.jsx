3530bc8609c15bb964b49cfb3a0b14d264b35c9f
Thyago Weber <thyago.weber@sevenapps.com.br>
2020-05-06 08:23:07 -0300

    feat/landing-page Adjusted Timeline animations

--- components/Atoms/Timeline/index.jsx
+++ components/Atoms/Timeline/index.jsx
@@ -3,15 +3,15 @@ import { Root, Item, Circle, Label } from './styles';
 
 export default function Timeline() {
   return (
-    <Root data-aos="slide-up" data-aos-offset="300">
-      <Item className="first">
-        <Label alignRight data-aos="fade-right" data-aos-offset="300">
+    <Root>
+      <Item data-aos="fade-in" data-aos-offset="300" className="first">
+        <Label alignRight>
           O salário de um profissional, pode ser o custo de um{' '}
           <strong>erro na contratação</strong>
         </Label>
       </Item>
-      <Item className="has-line first">
-        <Circle data-aos="fade-up" data-aos-offset="300">
+      <Item data-aos="fade-in" data-aos-offset="300" className="has-line first">
+        <Circle>
           3<span>x</span>
         </Circle>
       </Item>
@@ -20,26 +20,26 @@ export default function Timeline() {
       <Item>
         <Label></Label>
       </Item>
-      <Item className="has-line">
-        <Circle data-aos="fade-up" data-aos-offset="300">
+      <Item data-aos="fade-in" data-aos-offset="300" className="has-line">
+        <Circle>
           58<span>%</span>
         </Circle>
       </Item>
-      <Item>
-        <Label alignLeft data-aos="fade-left" data-aos-offset="300">
+      <Item data-aos="fade-in" data-aos-offset="300">
+        <Label>
           das contratações <strong>terminam em demissões</strong>, por erros na
           seleção de candidatos
         </Label>
       </Item>
 
-      <Item className="last">
-        <Label alignRight data-aos="fade-right" data-aos-offset="300">
+      <Item data-aos="fade-in" data-aos-offset="300" className="last">
+        <Label alignRight>
           dos profissionais <strong>desistem</strong> durante o processo de
           recrutamento
         </Label>
       </Item>
-      <Item className="has-line last">
-        <Circle data-aos="fade-up" data-aos-offset="300">
+      <Item data-aos="fade-in" data-aos-offset="300" className="has-line last">
+        <Circle>
           88<span>%</span>
         </Circle>
       </Item>
