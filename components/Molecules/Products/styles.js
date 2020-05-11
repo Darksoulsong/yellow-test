73e9c1580e5585cd609eefd48280d57375acd9bc
Thyago Weber <thyago.weber@sevenapps.com.br>
2020-05-11 17:28:57 -0300

    feat/landing-page faster circle animation

--- components/Molecules/Products/styles.js
+++ components/Molecules/Products/styles.js
@@ -11,8 +11,7 @@ export const CirclePathAnimation = css`
     animation-timing-function: ease-in-out;
     animation-fill-mode: forwards;
     animation-name: ${DrawLine};
-    animation-duration: 1s;
-    animation-delay: 0s;
+    animation-duration: 500ms;
     stroke-dasharray: 205;
     stroke-dashoffset: 205;
   }
@@ -66,8 +65,27 @@ export const Item = styled.div`
 
   &:hover {
     ${Description} {
-      transition-delay: 0.6s;
+      transition-delay: 0.25s;
       opacity: 1;
+    }
+  }
+`;
+
+export const ProductsActions = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 40px;
+  margin-top: -72px;
+
+  button > span {
+    font-weight: 300;
+    color: ${({ theme }) => theme.colors.white};
+    font-size: 1.8rem;
+    text-decoration: underline;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.yellow};
+      text-decoration: none;
     }
   }
 `;
