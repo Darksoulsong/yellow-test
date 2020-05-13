8f638ac901ce5f394dc9967e560614c3ec7e8e48
Thyago Weber <thyago.weber@sevenapps.com.br>
2020-05-13 10:12:53 -0300

    feat/create-account fixed intro footer img proportion

--- components/Atoms/Button/styles.js
+++ components/Atoms/Button/styles.js
@@ -24,10 +24,10 @@ const getButtonVariant = (variant, colors) => {
 
 const main = css`
   transition: transform 0.1s ease-in-out;
-  height: ${({ height = '87px' }) => height};
+  height: ${({ height = '78px' }) => height};
   padding: ${({ padding = '0 46px' }) => padding};
   color: ${({ theme }) => theme.colors.black};
-  font-size: ${({ fontSize = '2.5rem' }) => fontSize};
+  font-size: ${({ fontSize = '2.2rem' }) => fontSize};
   border: 0 none;
   background: ${({ theme }) => theme.colors.yellow};
   text-indent: -0.1rem;
@@ -36,6 +36,7 @@ const main = css`
   border-radius: 18px;
   transform: scale(1);
   white-space: nowrap;
+  letter-spacing: 0.063rem;
 
   ${({ theme, variant }) => getButtonVariant(variant, theme.colors)}
 
