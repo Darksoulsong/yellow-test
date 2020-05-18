6d25d09b1ac1ba66fffd8a4957d82c173c187900
Thyago Weber <thyago.weber@sevenapps.com.br>
2020-05-18 14:22:03 -0300

    feat/forgot-password removed unneeded files

--- components/Organisms/ForgotPasswordForm/views/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as AccountInfo } from './AccountInfo';
-export { default as PersonalInfoOne } from './PersonalInfoOne';
-export { default as PersonalInfoTwo } from './PersonalInfoTwo';
