8f3320c2f7b910e9b824150f23050d1ac268845c
Thyago Weber <thyago.weber@sevenapps.com.br>
2020-05-27 18:29:43 -0300

    feat/mobile-home-page removed unused fonts

--- components/Organisms/Theme/default/fonts.js
+++ components/Organisms/Theme/default/fonts.js
@@ -7,14 +7,6 @@ const style = css`
     font-weight: 400;
     src: url('fonts/monosten/Monosten-F.otf') format('opentype');
   }
-
-  @font-face {
-    font-family: 'Myriad Pro Regular';
-    font-style: normal;
-    font-weight: 400;
-    src: url('fonts/myriad_pro_regular/myriad_pro_regular.otf')
-      format('opentype');
-  }
 `;
 
 export default style;
